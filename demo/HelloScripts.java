94a0835fab909d35d83ae69684bdb81afc2fcdd1
Nicolai Parlog <nipa@codefx.org>
2019-04-12 19:28:57 +0200

    Make Caliz wrap Java 11 executable

--- demo/HelloScripts.java
+++ demo/HelloScripts.java
@@ -1,8 +1,12 @@
 // #! /home/nipa/code/Caliz/build/caliz
+import java.io.IOException;
+
 public class HelloScripts {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello, scripts!");
+		new ProcessBuilder("touch", "/home/nipa/code/Caliz/build/script-ran.txt").start();
+		System.exit(42);
 	}
 
 }
--- src/org/codefx/caliz/Caliz.java
+++ src/org/codefx/caliz/Caliz.java
@@ -1,12 +1,32 @@
 // #! /usr/bin/java11 --source 11
 package org.codefx.caliz;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Caliz {
 
-	public static void main(String[] args) {
-		Arrays.stream(args).forEach(System.out::println);
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO check args and maybe handle options beyond file to compile
+		String scriptPath = createScriptPath(args[0]);
+		Process script = startProcess(scriptPath);
+		script.waitFor();
+		System.out.println("Wohoo! " + script.exitValue());
+	}
+
+	private static String createScriptPath(String arg) {
+		String userDir = System.getProperty("user.dir");
+		Path script = Paths.get(userDir).resolve(arg);
+		return script.toAbsolutePath().toString();
+	}
+
+	private static Process startProcess(String scriptPath) throws IOException {
+		// TODO make path to JVM executable configurable
+		// TODO make source level configurable
+		return new ProcessBuilder("/usr/bin/java11", "--source 11", scriptPath)
+				.inheritIO()
+				.start();
 	}
 
 }
